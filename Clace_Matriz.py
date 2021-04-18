a2d8aa67ecbb3ab676ed2c4b2212d3af1c96048a
Jose Orozco <josemariaorozcosossa61@gmail.com>
2021-04-18 15:01:00 -0400

    Demostracion funcion Matriz Inversa

--- Clace_Matriz.py
+++ Clace_Matriz.py
@@ -296,5 +296,8 @@ class Matriz_Cuadrada(Matriz):
 
 if __name__ == '__main__':
     a = Matriz_Cuadrada('A', 3)
-    a.elementos_por_teclado()
+    a.elem_Random()
+
+    a_inv = Matriz_Cuadrada('A^-1', 3)
+    a.agregar_Inversa(a)
     a.imprimir_Matriz()
